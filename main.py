41688a6c445d58b0f437f6a155b0be82373197be
Aly Khaled <alykhaled2001@live.com>
2022-12-02 15:22:39 +0200

    Display function using Matplotlib and Pillow

--- main.py
+++ main.py
@@ -2,43 +2,130 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import scipy.io
-
+import PIL
+from PIL import ImageDraw
+from map import initial_map
 
 def load_from_file(filename):
     """
-    #TODO: Function to load a map from a matlab file.
+    Function to load a map from a matlab file.
 
     Params:
         filename: string of filename
     Returns:
-        map: 2d numpy array of 0s and 1s
+        map: 2d numpy array of 0s and 1s and 2 (goal)
     """
     mat = scipy.io.loadmat(filename)
-    return mat['map']
+    # print(mat)
+    return np.array(mat['map'])
 
 
-def display_map(map, trajectory=[]):
+def display_map(map=[], trajectory=[]):
     """
     #TODO: Function to display a map and the calculated trajectory.
     (use a more colourful scheme to differentiate between trajectory and map)
 
     Params:
         map: 2d numpy array of 0s and 1s
+        trajectory: list of tuples of (row, col) indices
     """
 
-    # add code here
+    # Make color map matrix with same size as map
+    color_map = np.zeros((map.shape[0], map.shape[1], 3))
+    for row in range(map.shape[0]):
+        for col in range(map.shape[1]):
+            if map[row, col] == 1:
+                color_map[row, col] = [145/255, 145/255, 145/255]
+            elif map[row, col] == 2:
+                color_map[row, col] = [0, 1, 0]
+            else:
+                color_map[row, col] = [1, 1, 1]
+    
+    for row, col in trajectory:
+        color_map[row, col] = [1, 0, 0]
 
-    pass
+    table = plt.table(cellText=map,cellColours=color_map, loc=(0,0), cellLoc='center')
+    plt.axis('off')
+    # plt.show()
+    plt.savefig('my_plot.png')
+
+def display_map_using_pillow(map, trajectory=[]):
+    # Draw the map
+    img = PIL.Image.new('RGB', (map.shape[1], map.shape[0]), color = 'white')
+    pixels = img.load()
+
+    for i in range(img.size[0]):    
+        for j in range(img.size[1]):    
+            if map[j,i] == 1:
+                pixels[i,j] = (145, 145, 145)
+            elif map[j,i] == 2:
+                pixels[i,j] = (0, 255, 0)
+            else:
+                pixels[i,j] = (255, 255, 255)
+
+    # Draw the trajectory
+    for row, col in trajectory:
+        pixels[col, row] = (255, 0, 0)
+
+    # Enlarge the image
+    img = img.resize((img.size[0]*10, img.size[1]*10), PIL.Image.NEAREST)
+    
+    img.save('my_plot.png')
 
 
-def print_output(value_map, trajectory):
+
+
+def print_output(value_map=np.array([]), trajectory=[]):
     """
-    #TODO: Function to print the output in the required format.
+    Function to print the output in the required format.
+
+    Params:
+        value_map: 2d numpy array of values
+        trajectory: list of tuples of (row, col) indices
+
     """
 
-    # add code here
+    # creates a formatted string of the value map and trajectory
+    # prints it to the console and saves it to a file
 
-    pass
+    # loop through the value map and adding to string
+    str_maze = ""
+    str_maze += "value_map = \n \n"
+    for row in range(value_map.shape[0]):
+        for item in range(value_map.shape[1]):
+            # left aligned formatting
+            str_maze += "{:4}".format(value_map[row, item])
+
+        str_maze += "\n"
+
+    str_maze += "\n\n"
+
+    # looping through the trajectory and adding it to the string
+    str_trajectory = ""
+    str_trajectory += "trajectory = \n \n"
+
+    for row in trajectory:
+        for item in row:
+            # left aligned formatting
+            str_trajectory += "{:4}".format(item)
+        str_trajectory += "\n"
+
+    str_out = str_maze + str_trajectory
+
+    # checks if the value map is very large and only prints it in file if it is
+    if (np.size(value_map) > 1000):
+        print("Output too large to print to console. Saved to output.txt")
+        print("")
+        with open("output.txt", "w") as text_file:
+            text_file.write(str_out)
+
+        # prints the trajectory to the console regardless..
+        print(str_trajectory)
+
+    else:
+        print(str_out)
+
+        print("Output saved to output.txt")
 
 
 def wavefront_map(map, goal_row, goal_col):
@@ -65,10 +152,10 @@ def planner(map, start_row, start_col):
         value_map: 2d numpy array of values
         trajectory: list of tuples of (row, col) indices
     """
-    value_map = []
-    trajectory = []
 
-    # add code here :)
+    # PLACEHOLDERS, delete and replace with code
+    value_map = map
+    trajectory = []
 
     # find the goal location (search for 2)
 
@@ -80,7 +167,6 @@ def planner(map, start_row, start_col):
 
     # return the value map and trajectory in required format
 
-
     return value_map, trajectory
 
 
@@ -88,6 +174,7 @@ def generate_random_map():
     """
     #TODO: Function to generate a random map. for testing purposes. 
     """
+    map = np.array([])
 
     # add code here :)
 
@@ -96,7 +183,7 @@ def generate_random_map():
 
 def main_loop():
     """
-    TODO: Main loop to get user input and call the required functions.
+    Main loop to get user input and call the required functions.
     """
 
     print("Starting user input loop...")
@@ -104,8 +191,8 @@ def main_loop():
         print("Select an option:")
         print("1. Load map from file")
         print("2. Generate random map")
-        print("3. Exit")
-        option = input("Enter option: ")  
+        print("0. Exit")
+        option = input("Enter option: ")
 
         if option == "1":
             filename = input("Enter filename: ")
@@ -124,7 +211,7 @@ def main_loop():
             display_map(value_map, trajectory)
             print_output(value_map, trajectory)
 
-        elif option == "3":
+        elif option == "0":
             break
 
         else:
@@ -136,8 +223,15 @@ def main_loop():
 
 def debug_loop():
     matrix = load_from_file("maze.mat")
+<<<<<<< HEAD
     # display_map(matrix)
-    print(matrix)
+    trajectory = [[8,2],[8,3]]
+    display_map_using_pillow(np.array(matrix),trajectory)
+    # print(matrix)
+=======
+    print_output(value_map=matrix, trajectory=[])
 
-main_loop()
-# debug_loop()
+>>>>>>> 41a831e6089fe088072b4388168d4e03a2a2931d
+
+# main_loop()
+debug_loop()
